9ebbef9220d86e5e0e738983927f75a846e1fab9
Anders Børjesson <anbo@zealand.dk>
2021-02-16 11:04:04 +0100

    generic table Vue

---
+++ js/index.js
@@ -0,0 +1,21 @@
+const baseUrl = "http://jsonplaceholder.typicode.com/posts"
+//const baseUrl = "http://jsonplaceholder.typicode.com/users" 
+
+// Should work with any REST GET url that returns an array of objects
+
+Vue.createApp({
+    data() {
+        return {
+            data: [],
+            message: null
+        }
+    },
+    async created() {
+       try {
+           const response = await axios.get(baseUrl)
+           this.data = await response.data
+       } catch (error) {
+           this.message = error.message
+       }  
+    }
+}).mount("#app")
